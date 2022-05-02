2c6db8e86374509ee09dec8e52c5d9cd68b6a328
lhar gil <6247364+lhargil@users.noreply.github.com>
2022-05-02 13:00:41 +0800

    feat: add test1 app

---
+++ apps/test1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tst1-root',
+  template: ` <tst1-nx-welcome></tst1-nx-welcome> `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'test1';
+}
